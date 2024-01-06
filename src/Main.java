d92751fb04506357893b4995be28cb6b183a199d
leonelborgesdev <leonel03t@gmail.com>
2024-01-06 12:37:28 -0400

    ejemplos main

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,49 @@
+import clases.Producto;
+import filtros.Descartable;
+import filtros.Imperecedero;
+import filtros.Vendible;
+import interfaces.Filtrable;
+
+import java.util.Arrays;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Intro with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Crear array de productos
+        Producto[] productos = new Producto[] {
+                new Producto("Producto 1", 50.0, 1),
+                new Producto("Producto 2", 25.0, 2),
+                new Producto("Producto 3", 150.0, 3),
+                new Producto("Descartable 1", 5.0, 4),
+                new Producto("Descartable 2", 10.0, 5)
+        };
+        // Ordenar proudctos precio
+        Arrays.sort(productos);
 
-        // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        System.out.println("Array de productos ordenado por precio:");
+        for (Producto producto: productos){
+            System.out.println(producto);
+        };
+        System.out.println("\nProductos que cumplen con el filtro 'Vendible':");
+        Filtrable vendibleFiltro=new Vendible();
+        filtrarProducttos(productos, vendibleFiltro);
 
-            // Press Mayús+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        System.out.println("\nProductos que cumplen con el filtro 'Descartable:'");
+        Filtrable descartableFiltro = new Descartable();
+        filtrarProducttos(productos, descartableFiltro);
+
+        System.out.println("\nProductos que cumplen con el filtro 'Imperecedero'");
+        Filtrable imperecedroFiltro= new Imperecedero();
+        filtrarProducttos(productos, imperecedroFiltro);
+    }
+
+    private static void filtrarProducttos(Producto[] productos, Filtrable filtro) {
+        System.out.println("Productos que cumplen con el filtro '"+ filtro.getClass().getSimpleName()+"':");
+        for (Producto producto:productos){
+            if(filtro.cumpleFiltro(producto)){
+                System.out.println(producto);
+            }
         }
     }
 }
